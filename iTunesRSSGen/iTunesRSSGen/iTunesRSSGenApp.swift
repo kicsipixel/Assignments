a86791b71be9393e93e6297d5357aff80d0ff527
Szabolcs Toth <tsz@purzelbaum.hu>
2021-12-21 10:50:36 +0100

    Added iTunesRSSGen project.

---
+++ iTunesRSSGen/iTunesRSSGen/iTunesRSSGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesRSSGenApp.swift
+//  iTunesRSSGen
+//
+//  Created by Szabolcs Tóth on 2021. 12. 21..
+//
+
+import SwiftUI
+
+@main
+struct iTunesRSSGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
