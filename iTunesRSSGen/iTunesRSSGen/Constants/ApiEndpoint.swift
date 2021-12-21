2da259e2720162e6621fef09287f18c27188c9dc
Szabolcs Toth <tsz@purzelbaum.hu>
2021-12-21 10:52:55 +0100

    Added ApeEndPoint.

---
+++ iTunesRSSGen/iTunesRSSGen/Constants/ApiEndpoint.swift
@@ -0,0 +1,12 @@
+//
+//  ApiEndpoint.swift
+//  iTunesRSSGen
+//
+//  Created by Szabolcs Tóth on 2021. 12. 21..
+//
+
+import Foundation
+
+struct ApiEndpoint {
+    static let urlString: String = "https://rss.applemarketingtools.com/api/v2/us/music/most-played/50/albums.json"
+}
